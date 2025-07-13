e9a2c00a2848bc87caa5ae76eeef249e8e14d3ed
Deepakshrivas078 <dshrivas237@gmail.com>
2025-07-13 22:08:51 +0530

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "LeadStrike AI Backend is running 🚀" });
+  res.status(200).json({ message: "Hello from backend!" });
 }
